7ac9f203b11ad6ff2bfd21abcfee7d834e6eb5d3
Andre Carter <carstar2486@gmail.com>
2020-03-29 21:25:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians,instruments) {
 function johnLennonFacts(array) {
   var i = 0;
   while (i < array.length) {
-    array[i] = array[i] + '!!!';
+    array[i] += '!!!';
     i++;
   }
   return array;
